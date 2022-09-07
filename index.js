89ad4760b33f3d5a4a1dfe003ed76a5ffe574182
TheAM01 <official.theammusic@gmail.com>
2022-09-07 14:17:14 +0500

    Added express server

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import client from "./Main/client.js";
 import commandHandler from "./Main/handler.js";
+import server from "./Main/server.js";
 
 client.on('ready', (c) => {
     console.clear();
     console.log(`Logged in as ${c.user.username}!`);
+    server()
 });
 
 client.on('interactionCreate', commandHandler)
